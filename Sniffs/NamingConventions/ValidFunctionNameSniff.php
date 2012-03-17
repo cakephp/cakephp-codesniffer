b3da29272de067aa7b631660d8400c24a43d9f70
Juan Basso <jrbasso@gmail.com>
2012-03-16 22:26:40 -0400

    Allowing underscored public methods in controllers. Fix #2

--- Sniffs/NamingConventions/ValidFunctionNameSniff.php
+++ Sniffs/NamingConventions/ValidFunctionNameSniff.php
@@ -128,6 +128,10 @@ class CakePHP_Sniffs_NamingConventions_ValidFunctionNameSniff extends PHP_CodeSn
 				$phpcsFile->addError($error, $stackPtr, 'PublicWithUnderscore', $errorData);
 				return;
 			}
+            // Underscored public methods in controller are allowed to break our rules.
+            if (substr($className, -10) === 'Controller') {
+                return;
+            }
 		} elseif ($isPrivate === true) {
             if (substr($methodName, 0, 2) !== '__') {
                 $error = 'Private method name "%s" must be prefixed with 2 underscores';
