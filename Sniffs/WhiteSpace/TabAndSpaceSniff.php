3fcbe57fc00a40f3ed7f31ac2ebc979909ceedbd
Hiroaki Sone <koutetsu666@gmail.com>
2013-05-02 01:21:31 +0900

    Revert "インデントはタブではなく、スペースで行う"
    This reverts commit 59959e1becb561ef4ddf8cdf8f6bc4376af41b0e.

--- Sniffs/WhiteSpace/TabAndSpaceSniff.php
+++ Sniffs/WhiteSpace/TabAndSpaceSniff.php
@@ -51,24 +51,18 @@ class CakePHP_Sniffs_WhiteSpace_TabAndSpaceSniff implements PHP_CodeSniffer_Snif
 			return;
 		}
 
-		if (strpos($tokens[$stackPtr]['content'], "\t") !== false) {
-			$error = 'Tab found';
+		if (strpos($tokens[$stackPtr]['content'], "  ") !== false) {
+			$error = 'Double space found';
 			$phpcsFile->addError($error, $stackPtr);
-                }
-                
-
-//		if (strpos($tokens[$stackPtr]['content'], "  ") !== false) {
-//			$error = 'Double space found';
-//			$phpcsFile->addError($error, $stackPtr);
-//		}
-//		if (strpos($tokens[$stackPtr]['content'], " \t") !== false) {
-//			$error = 'Space and tab found';
-//			$phpcsFile->addError($error, $stackPtr);
-//		}
-//		if (strpos($tokens[$stackPtr]['content'], "\t ") !== false) {
-//			$error = 'Tab and space found';
-//			$phpcsFile->addError($error, $stackPtr);
-//		}
+		}
+		if (strpos($tokens[$stackPtr]['content'], " \t") !== false) {
+			$error = 'Space and tab found';
+			$phpcsFile->addError($error, $stackPtr);
+		}
+		if (strpos($tokens[$stackPtr]['content'], "\t ") !== false) {
+			$error = 'Tab and space found';
+			$phpcsFile->addError($error, $stackPtr);
+		}
 	}
 
 }
