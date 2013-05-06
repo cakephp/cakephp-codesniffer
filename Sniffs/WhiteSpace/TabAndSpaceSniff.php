db1a688c9aa782496e5fb5e8ff406fcffddd26e8
Hiroaki Sone <koutetsu666@gmail.com>
2013-05-06 20:57:45 +0900

    Double Spaceを警告にしない

--- Sniffs/WhiteSpace/TabAndSpaceSniff.php
+++ Sniffs/WhiteSpace/TabAndSpaceSniff.php
@@ -51,10 +51,10 @@ class CakePHP_Sniffs_WhiteSpace_TabAndSpaceSniff implements PHP_CodeSniffer_Snif
 			return;
 		}
 
-		if (strpos($tokens[$stackPtr]['content'], "  ") !== false) {
-			$error = 'Double space found';
-			$phpcsFile->addError($error, $stackPtr);
-		}
+//		if (strpos($tokens[$stackPtr]['content'], "  ") !== false) {
+//			$error = 'Double space found';
+//			$phpcsFile->addError($error, $stackPtr);
+//		}
 		if (strpos($tokens[$stackPtr]['content'], " \t") !== false) {
 			$error = 'Space and tab found';
 			$phpcsFile->addError($error, $stackPtr);
