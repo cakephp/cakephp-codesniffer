ded791734f482ebebe50f18ac21bfd6055e6adad
Brian Porter <beporter@users.sourceforge.net>
2014-04-05 10:38:17 -0500

    Renamed test files for consistency.

---
+++ tests/files/Loadsys/throws_pass.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Loadsys;
+
+use Other\Crap;
+use Other\Error as OtherError;
+
+class Throws {
+
+	/**
+	 * Test throws
+	 *
+	 * @throws Exception
+	 * @throws Loadsys\Boom
+	 * @throws Loadsys\Error\Boom
+	 * @throws Other\Crap
+	 * @throws Other\Error\Issue
+	 */
+	public function test() {
+		switch ($a) {
+			case 1:
+				throw new Boom();
+			case 2:
+				throw new Error\Boom();
+			case 3:
+				throw new OtherError\Issue();
+			case 4:
+				throw new Crap();
+			default:
+				throw new \Exception();
+		}
+	}
+
+}
