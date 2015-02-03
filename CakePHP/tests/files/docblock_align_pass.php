1ac27edb931c4ecad93fb247e925530ca7703b1d
Bryan Crowe <bryan@bryan-crowe.com>
2015-02-02 21:24:22 -0500

    Add docblock_align pass/fail files

---
+++ CakePHP/tests/files/docblock_align_pass.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Hello World.
+ *
+ * This is a description.
+ */
+class Foo extends Bar
+{
+    /**
+     * What are your thoughts?
+     *
+     * @var array $brain
+     */
+    public $brain = array();
+
+    /**
+     * Tell me your thoughts.
+     *
+     * @return void
+     */
+    public function dumpThoughts()
+    {
+        foreach ($thoughts as $thought) {
+            echo $thought;
+        }
+    }
+}
