29683ea5b61296126afdc5a9b70581e44226aefc
ADmad <admad.coder@gmail.com>
2015-03-11 11:50:01 +0530

    Add test for fix done in 07b6929561

--- CakePHP/tests/files/function_comment_type_pass.php
+++ CakePHP/tests/files/function_comment_type_pass.php
@@ -13,4 +13,30 @@ class Foo
     public function bar($param, $otherParam)
     {
     }
+
+    /**
+     * Description
+     *
+     * @return mixed
+     */
+    public function baz()
+    {
+        if ($something) {
+            return;
+        }
+        return 'foo';
+    }
+
+    /**
+     * Description
+     *
+     * @return void|string
+     */
+    public function foo()
+    {
+        if ($something) {
+            return;
+        }
+        return 'foo';
+    }
 }
