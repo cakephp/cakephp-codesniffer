6808b9559fa0a38c88a6738c4fcedc337b3742fe
Walther Lalk <dakota@users.noreply.github.com>
2016-03-08 10:28:37 +0200

    Integer -> int

--- CakePHP/Sniffs/NamingConventions/ValidClassBracketsSniff.php
+++ CakePHP/Sniffs/NamingConventions/ValidClassBracketsSniff.php
@@ -36,7 +36,7 @@ class CakePHP_Sniffs_NamingConventions_ValidClassBracketsSniff implements PHP_Co
  * Processes this test, when one of its tokens is encountered.
  *
  * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
- * @param integer $stackPtr  The position of the current token in the stack passed in $tokens.
+ * @param int $stackPtr  The position of the current token in the stack passed in $tokens.
  * @return void
  */
     public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
