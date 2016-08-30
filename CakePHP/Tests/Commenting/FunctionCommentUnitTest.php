999fece5bb0f1e53d4016c3303ce99692010e000
antograssiot <antograssiot@free.fr>
2016-08-30 06:53:32 +0200

    continue to improve code coverage

--- CakePHP/Tests/Commenting/FunctionCommentUnitTest.php
+++ CakePHP/Tests/Commenting/FunctionCommentUnitTest.php
@@ -14,6 +14,16 @@ class FunctionCommentUnitTest extends AbstractSniffUnitTest
         return [
             12 => 1,
             13 => 1,
+            23 => 1,
+            24 => 1,
+            34 => 1,
+            35 => 1,
+            90 => 1,
+            97 => 1,
+            104 => 1,
+            112 => 1,
+            222 => 1,
+            231 => 1,
         ];
     }
 
@@ -24,7 +34,21 @@ class FunctionCommentUnitTest extends AbstractSniffUnitTest
     {
         return [
             14 => 1,
-            23 => 1,
+            31 => 2,
+            45 => 1,
+            140 => 1,
+            145 => 1,
+            155 => 1,
+            165 => 1,
+            174 => 1,
+            182 => 1,
+            190 => 1,
+            197 => 1,
+            198 => 1,
+            205 => 1,
+            206 => 1,
+            215 => 1,
+            221 => 1,
         ];
     }
 }
