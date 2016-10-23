c956809c546bcda603e498ad81add208cc56f184
Narendra Vaghela <narendravaghela4389@gmail.com>
2016-10-24 00:01:42 +0530

    Fix #158

--- CakePHP/Sniffs/WhiteSpace/CommaSpacingSniff.php
+++ CakePHP/Sniffs/WhiteSpace/CommaSpacingSniff.php
@@ -47,6 +47,11 @@ class CakePHP_Sniffs_WhiteSpace_CommaSpacingSniff implements PHP_CodeSniffer_Sni
         $next = $phpcsFile->findNext(T_WHITESPACE, ($stackPtr + 1), null, true);
 
         if ($tokens[$next]['code'] !== T_WHITESPACE && ($next !== $stackPtr + 2)) {
+            // Skip if immediate char is comma
+            if ($tokens[$next]['code'] === T_COMMA) {
+                return;
+            }
+
             // Last character in a line is ok.
             if ($tokens[$next]['line'] === $tokens[$stackPtr]['line']) {
                 $error = 'Missing space after comma';
