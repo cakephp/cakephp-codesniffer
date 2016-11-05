631680d13f55a4955a5266701787d87e79d1e336
Mark Story <mark@mark-story.com>
2016-11-05 09:55:22 -0400

    Add test coverage for #174

--- CakePHP/tests/files/whitespace_comma_pass.php
+++ CakePHP/tests/files/whitespace_comma_pass.php
@@ -1,2 +1,3 @@
 <?php
-    echo implode('', array_map('chr', array_map('hexdec', array_filter(explode($delimiter, $string)))));
+echo implode('', array_map('chr', array_map('hexdec', array_filter(explode($delimiter, $string)))));
+list(,, $extension, $filename) = array_values(pathinfo($filename));
