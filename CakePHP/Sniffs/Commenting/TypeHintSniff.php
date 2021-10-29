0c1c12ac7b1f8f5cf72b516676f0ff7f828c30dd
mscherer <mark.scherer@spryker.com>
2021-10-29 11:01:38 +0200

    Improve TypeHint sniff.

--- CakePHP/Sniffs/Commenting/TypeHintSniff.php
+++ CakePHP/Sniffs/Commenting/TypeHintSniff.php
@@ -34,7 +34,7 @@ use PHPStan\PhpDocParser\Parser\TokenIterator;
 use PHPStan\PhpDocParser\Parser\TypeParser;
 
 /**
- * Verifies order of types in type hints
+ * Verifies order of types in type hints. Also removes duplicates.
  */
 class TypeHintSniff implements Sniff
 {
@@ -59,9 +59,42 @@ class TypeHintSniff implements Sniff
     ];
 
     /**
+     * Highest/First element will be first in list of param or return tag.
+     *
+     * @var array<string>
+     */
+    protected static $sortMap = [
+        '\\Closure',
+        '\\Traversable',
+        '\\ArrayAccess',
+        '\\ArrayObject',
+        '\\Stringable',
+        '\\Generator',
+        'mixed',
+        'callable',
+        'resource',
+        'object',
+        'iterable',
+        'list',
+        'array',
+        'callable-string',
+        'class-string',
+        'interface-string',
+        'scalar',
+        'string',
+        'float',
+        'int',
+        'bool',
+        'true',
+        'false',
+        'null',
+        'void',
+    ];
+
+    /**
      * Returns an array of tokens this test wants to listen for.
      *
-     * @return array
+     * @inheritDoc
      */
     public function register()
     {
@@ -71,9 +104,7 @@ class TypeHintSniff implements Sniff
     /**
      * Processes this test, when one of its tokens is encountered.
      *
-     * @param \PHP_CodeSniffer\Files\File $phpcsFile The file being scanned.
-     * @param int $stackPtr The position of the current token in the stack passed in $tokens.
-     * @return void
+     * @inheritDoc
      */
     public function process(File $phpcsFile, $stackPtr)
     {
@@ -86,19 +117,22 @@ class TypeHintSniff implements Sniff
         foreach ($tokens[$stackPtr]['comment_tags'] as $tag) {
             if (
                 $tokens[$tag + 2]['code'] !== T_DOC_COMMENT_STRING ||
-                !in_array($tokens[$tag]['content'], self::$typeHintTags, true)
+                !in_array($tokens[$tag]['content'], static::$typeHintTags, true)
             ) {
                 continue;
             }
 
             $tagComment = $phpcsFile->fixer->getTokenContent($tag + 2);
-            $valueNode = self::getValueNode($tokens[$tag]['content'], $tagComment);
+            $valueNode = static::getValueNode($tokens[$tag]['content'], $tagComment);
             if ($valueNode instanceof InvalidTagValueNode) {
                 continue;
             }
 
+            $hasUnion = false;
+            /** @var \PHPStan\PhpDocParser\Ast\PhpDoc\PropertyTagValueNode|\PHPStan\PhpDocParser\Ast\PhpDoc\VarTagValueNode|\PHPStan\PhpDocParser\Ast\PhpDoc\ParamTagValueNode|\PHPStan\PhpDocParser\Ast\PhpDoc\ReturnTagValueNode $valueNode */
             if ($valueNode->type instanceof UnionTypeNode) {
                 $types = $valueNode->type->types;
+                $hasUnion = true;
             } elseif ($valueNode->type instanceof ArrayTypeNode) {
                 $types = [$valueNode->type];
             } else {
@@ -106,12 +140,12 @@ class TypeHintSniff implements Sniff
             }
 
             $originalTypeHint = $this->renderUnionTypes($types);
-            $sortedTypeHint = $this->getSortedTypeHint($types);
+            $sortedTypeHint = $this->getSortedTypeHint($types, $hasUnion);
             if ($sortedTypeHint === $originalTypeHint) {
                 continue;
             }
 
-            $fix = $phpcsFile->addFixableWarning(
+            $fix = $phpcsFile->addFixableError(
                 '%s type hint is not formatted properly, expected "%s"',
                 $tag,
                 'IncorrectFormat',
@@ -156,40 +190,12 @@ class TypeHintSniff implements Sniff
     }
 
     /**
-     * @param array $types node types
+     * @param array<\PHPStan\PhpDocParser\Ast\Type\TypeNode> $types node types
      * @return string
      */
-    protected function getSortedTypeHint(array $types): string
+    protected function getSortedTypeHint(array $types, bool $hasUnion): string
     {
-        static $shouldSort = [
-            '\\Closure',
-            '\\Traversable',
-            '\\ArrayAccess',
-            '\\ArrayObject',
-            '\\Stringable',
-            '\\Generator',
-            'mixed',
-            'callable',
-            'resource',
-            'object',
-            'iterable',
-            'list',
-            'array',
-            'callable-string',
-            'class-string',
-            'interface-string',
-            'scalar',
-            'string',
-            'float',
-            'int',
-            'bool',
-            'true',
-            'false',
-            'null',
-            'void',
-        ];
-
-        $sortable = array_fill_keys($shouldSort, []);
+        $sortable = array_fill_keys(static::$sortMap, []);
         $unsortable = [];
         foreach ($types as $type) {
             $sortName = null;
@@ -200,28 +206,33 @@ class TypeHintSniff implements Sniff
                     $sortName = $type->type->name;
                 }
             } elseif ($type instanceof ArrayTypeNode) {
-                if ($this->convertArraysToGenerics) {
+                if (!$this->convertArraysToGenerics) {
+                    $sortName = $type->type->name;
+                } elseif (!$this->isComplexObjectCollection($types, $hasUnion)) {
                     $type = new GenericTypeNode(new IdentifierTypeNode('array'), [$type->type]);
                     $sortName = 'array';
                 } elseif ($type->type instanceof IdentifierTypeNode) {
-                    $sortName = $type->type->name;
+                    $sortName = 'array';
                 } else {
                     $sortName = 'array';
                 }
             } elseif ($type instanceof ArrayShapeNode) {
                 $sortName = 'array';
             } elseif ($type instanceof GenericTypeNode) {
-                if (in_array($type->type->name, $shouldSort)) {
+                if (in_array($type->type->name, static::$sortMap)) {
                     $sortName = $type->type->name;
+                } else {
+                    $sortName = 'array';
                 }
             }
 
             if (!$sortName) {
                 $unsortable[] = $type;
+
                 continue;
             }
 
-            if (in_array($sortName, $shouldSort, true)) {
+            if (in_array($sortName, static::$sortMap, true)) {
                 if ($type instanceof ArrayTypeNode) {
                     array_unshift($sortable[$sortName], $type);
                 } else {
@@ -233,11 +244,14 @@ class TypeHintSniff implements Sniff
         }
 
         $sorted = [];
-        array_walk($sortable, function ($types) use (&$sorted) {
+        array_walk($sortable, function ($types) use (&$sorted): void {
             $sorted = array_merge($sorted, $types);
         });
 
-        return $this->renderUnionTypes(array_merge($unsortable, $sorted));
+        $types = array_merge($unsortable, $sorted);
+        $types = $this->makeUnique($types);
+
+        return $this->renderUnionTypes($types);
     }
 
     /**
@@ -246,7 +260,7 @@ class TypeHintSniff implements Sniff
      */
     protected function renderUnionTypes(array $typeNodes): string
     {
-        return preg_replace(
+        return (string)preg_replace(
             ['/ ([\|&]) /', '/<\(/', '/\)>/'],
             ['${1}', '<', '>'],
             implode('|', $typeNodes)
@@ -258,7 +272,7 @@ class TypeHintSniff implements Sniff
      * @param string $tagComment tag comment
      * @return \PHPStan\PhpDocParser\Ast\PhpDoc\PhpDocTagValueNode
      */
-    protected static function getValueNode(string $tagName, $tagComment): PhpDocTagValueNode
+    protected static function getValueNode(string $tagName, string $tagComment): PhpDocTagValueNode
     {
         static $phpDocParser;
         if (!$phpDocParser) {
@@ -272,5 +286,48 @@ class TypeHintSniff implements Sniff
         }
 
         return $phpDocParser->parseTagValue(new TokenIterator($phpDocLexer->tokenize($tagComment)), $tagName);
+    }
+
+    /**
+     * @param array<\PHPStan\PhpDocParser\Ast\Type\TypeNode> $types
+     * @return array<\PHPStan\PhpDocParser\Ast\Type\TypeNode>
+     */
+    protected function makeUnique(array $types): array
+    {
+        $typesAsString = [];
+
+        foreach ($types as $key => $type) {
+            $type = (string)$type;
+            if (in_array($type, $typesAsString, true)) {
+                unset($types[$key]);
+
+                continue;
+            }
+            $typesAsString[] = $type;
+        }
+
+        return $types;
+    }
+
+    /**
+     * @param array<\PHPStan\PhpDocParser\Ast\Type\TypeNode> $types
+     * @return bool
+     */
+    protected function isComplexObjectCollection(array $types, bool $isUnion): bool
+    {
+        if (!$isUnion) {
+        }
+
+        foreach ($types as $type) {
+            if (!$type instanceof IdentifierTypeNode) {
+                continue;
+            }
+
+            if (strpos((string)$type, '\\') === 0) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
